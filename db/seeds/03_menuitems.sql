2a12f7adab6ef7568a39b1fe155448fa39d623d0
Ning Li <lining04111223@gmail.com>
2022-10-24 16:32:16 +0000

    add db

---
+++ db/seeds/03_menuitems.sql
@@ -0,0 +1,14 @@
+INSERT INTO menuitems (restaurant_id, name, description, price, picture, vegetarian)
+VALUES
+(1, 'Burger', 'Beef Burger', 10, 'https://food.fnr.sndimg.com/content/dam/images/food/products/2022/3/11/rx_goldbelly-clinton-street-diner-zeus-burger.jpg.rend.hgtvcom.406.305.suffix/1647019464547.jpeg',FALSE),
+(1, 'Turkey', 'Turkey', 40, 'https://pngimg.com/uploads/turkey_food/turkey_food_PNG14.png',FALSE),
+(1, 'Pizza', 'Cheese Pizza', 18, 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2016/8/23/0/FNM_100116-Classic-Crust_s4x3.jpg.rend.hgtvcom.616.462.suffix/1480972867043.jpeg',FALSE),
+(1, 'Noodle', 'Dan Dan Noodles', 18, 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dan-dan-noodles-1647627163.jpeg?crop=0.668xw:1.00xh;0.211xw,0&resize=980:*',FALSE),
+(1, 'Tofu', 'Mapo Tofu', 15, 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ma-po-tofu-1647625672.jpeg?crop=0.6675347222222222xw:1xh;center,top&resize=980:*', TRUE),
+(1, 'Soup Dumplings', 'Xiao Long Bao', 10, 'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/2941f28b-e7f6-43e1-a383-cc2a44f7378f/Derivates/3550132d-1573-4fdf-873f-bfe282aff856.jpg',FALSE),
+(1, 'Chinese Hamburger', 'Rou Jia Mo', 10, 'https://publish.purewow.net/wp-content/uploads/sites/2/2020/02/traditional-chinese-food-chinese-hamburger.jpg?fit=728%2C921',FALSE),
+(1, 'Spring Rolls', 'Chun Juan', 10, 'https://publish.purewow.net/wp-content/uploads/sites/2/2021/12/traditional-chinese-food-spring-rolls.jpg?fit=728%2C921',TRUE),
+(1, 'HOT POT', 'Huo Guo', 60, 'https://i.cbc.ca/1.3798472.1534527779!/fileImage/httpImage/image.JPG_gen/derivatives/original_780/hot-pot.JPG',FALSE);
+
+
+
